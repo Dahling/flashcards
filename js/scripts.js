dbc621c89c5f8e404bc93aad18b7fce258678d5c
Beth Hansen and Jenna Cooper <jenna@>
2017-04-25 16:46:21 -0700

    Add everything we created to try and make flashcards

---
+++ js/scripts.js
@@ -0,0 +1,159 @@
+$(document).ready(function() {
+  $(".js-showing").click(function() {
+    $(".js-showing").toggle();
+    $(".js-hidden").toggle();
+  });
+  $(".js-hidden").click(function() {
+    $(".js-showing").toggle();
+    $(".js-hidden").toggle();
+  });
+  $(".op-showing").click(function() {
+    $(".op-showing").toggle();
+    $(".op-hidden").toggle();
+    });
+  $(".op-hidden").click(function() {
+    $(".op-showing").toggle();
+    $(".op-hidden").toggle();
+    });
+  $(".var-showing").click(function() {
+    $(".var-showing").toggle();
+    $(".var-hidden").toggle();
+    });
+  $(".var-hidden").click(function() {
+    $(".var-showing").toggle();
+    $(".var-hidden").toggle();
+    });
+  $(".vnc-showing").click(function() {
+    $(".vnc-showing").toggle();
+    $(".vnc-hidden").toggle();
+    });
+  $(".vnc-hidden").click(function() {
+    $(".vnc-showing").toggle();
+    $(".vnc-hidden").toggle();
+    });
+  $(".fun-showing").click(function() {
+    $(".fun-showing").toggle();
+    $(".fun-hidden").toggle();
+    });
+  $(".fun-hidden").click(function() {
+    $(".fun-showing").toggle();
+    $(".fun-hidden").toggle();
+    });
+  $(".meth-showing").click(function() {
+    $(".meth-showing").toggle();
+    $(".meth-hidden").toggle();
+    });
+  $(".meth-hidden").click(function() {
+    $(".meth-showing").toggle();
+    $(".meth-hidden").toggle();
+    });
+  $(".argu-showing").click(function() {
+    $(".argu-showing").toggle();
+    $(".argu-hidden").toggle();
+    });
+  $(".argu-hidden").click(function() {
+    $(".argu-showing").toggle();
+    $(".argu-hidden").toggle();
+    });
+  $(".para-showing").click(function() {
+    $(".para-showing").toggle();
+    $(".para-hidden").toggle();
+    });
+  $(".para-hidden").click(function() {
+    $(".para-showing").toggle();
+    $(".para-hidden").toggle();
+    });
+  // $(".re-showing").click(function() {
+  //   $(".re-showing").toggle();
+  //   $(".re-hidden").toggle();
+  // });
+  // $(".re-hidden").click(function() {
+  //   $(".re-showing").toggle();
+  //   $(".re-hidden").toggle();
+  // });
+  // $(".cm-showing").click(function() {
+  //   $(".cm-showing").toggle();
+  //   $(".cm-hidden").toggle();
+  //   });
+  // $(".cm-hidden").click(function() {
+  //   $(".cm-showing").toggle();
+  //   $(".cm-hidden").toggle();
+  //   });
+  // $(".strings-showing").click(function() {
+  //   $(".strings-showing").toggle();
+  //   $(".strings-hidden").toggle();
+  //   });
+  // $(".strings-hidden").click(function() {
+  //   $(".strings-showing").toggle();
+  //   $(".strings-hidden").toggle();
+  //   });
+  // $(".boo-showing").click(function() {
+  //   $(".boo-showing").toggle();
+  //   $(".boo-hidden").toggle();
+  //   });
+  // $(".boo-hidden").click(function() {
+  //   $(".boo-showing").toggle();
+  //   $(".boo-hidden").toggle();
+  //   });
+  // $(".un-showing").click(function() {
+  //   $(".un-showing").toggle();
+  //   $(".un-hidden").toggle();
+  //   });
+  // $(".un-hidden").click(function() {
+  //   $(".un-showing").toggle();
+  //   $(".un-hidden").toggle();
+  //   });
+  // $(".nan-showing").click(function() {
+  //   $(".nan-showing").toggle();
+  //   $(".nan-hidden").toggle();
+  //   });
+  // $(".nan-hidden").click(function() {
+  //   $(".nan-showing").toggle();
+  //   $(".nan-hidden").toggle();
+  //   });
+  // $(".es-showing").click(function() {
+  //   $(".es-showing").toggle();
+  //   $(".es-hidden").toggle();
+  //   });
+  // $(".es-hidden").click(function() {
+  //   $(".es-showing").toggle();
+  //   $(".es-hidden").toggle();
+  //   });
+  // $(".alert-showing").click(function() {
+  //   $(".alert-showing").toggle();
+  //   $(".alert-hidden").toggle();
+  //   });
+  // $(".alert-hidden").click(function() {
+  //   $(".alert-showing").toggle();
+  //   $(".alert-hidden").toggle();
+  //   });
+  // $(".com-showing").click(function() {
+  //   $(".com-showing").toggle();
+  //   $(".com-hidden").toggle();
+  //   });
+  // $(".com-hidden").click(function() {
+  //   $(".com-showing").toggle();
+  //   $(".com-hidden").toggle();
+  //   });
+  // $(".j-showing").click(function() {
+  //   $(".j-showing").toggle();
+  //   $(".j-hidden").toggle();
+  //   });
+  // $(".j-hidden").click(function() {
+  //   $(".j-showing").toggle();
+  //   $(".j-hidden").toggle();
+  //   });
+  // $(".att-showing").click(function() {
+  //   $(".att-showing").toggle();
+  //   $(".att-hidden").toggle();
+  //   });
+  // $(".att-hidden").click(function() {
+  //   $(".att-showing").toggle();
+  //   $(".att-hidden").toggle();
+  //   });
+  //
+
+
+
+
+    });
